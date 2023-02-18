05528c4ef155686ea978d5c47fbb09d8f14c7f56
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2023-02-18 16:54:44 +0300

    Add application bootstrap

---
+++ application/src/main/java/jira/javarush/com/JiraRushApplication.java
@@ -0,0 +1,11 @@
+package jira.javarush.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraRushApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JiraRushApplication.class, args);
+    }
+}
